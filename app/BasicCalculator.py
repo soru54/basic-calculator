32625c74b5970d7e7bfa091bea08fd6aea9057d0
Soru54 <84532434+soru54@users.noreply.github.com>
2024-11-23 22:33:34 -0300

    Create BasicCalculator.py

---
+++ app/BasicCalculator.py
@@ -0,0 +1,27 @@
+print('==============BASIC=CALCULATOR===========================')
+n1 = float(input('Choose a number: '))
+n2 = float(input('Choose one more number: '))
+s = n1+n2
+su = n1-n2
+mu = n1*n2
+d = n1/n2
+di = n1//n2
+power = n1**n2
+rqn1 = n1**(1/2)
+rqn2 = n2**(1/2)
+rcn1 = n1**(1/3)
+rcn2 = n2**(1/3)
+r4n1 = n1**(1/4)
+r4n2 = n2**(1/4)
+print('The sum of {} and {} is {},'.format(n1, n2, s))
+print('The subtraction is {}, the product is {},'.format(su, mu))
+print('The division is {}, the integer division is {},'.format(d, di))
+print('{} to the power of {} equals {},'.format(n1, n2, power))
+print('The square root of {} equals {},'.format(n1, rqn1))
+print('The square root of {} equals {},'.format(n2, rqn2))
+print('The cubic root of {} equals {},'.format(n1, rcn1))
+print('The cubic root of {} equals {},'.format(n2, rcn2))
+print('The fourth root of {} equals {} and'.format(n1, r4n1))
+print('The fourth root of {} equals {}.'.format(n2, r4n2))
+print('=========================================================')
+print('                By Soru54, in 2024.')
