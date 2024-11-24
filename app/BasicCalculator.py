4b2405df163af1ce8487fc1fe30bbc090451f2ac
Soru54 <84532434+soru54@users.noreply.github.com>
2024-11-24 13:51:18 -0300

    Edited code

--- app/BasicCalculator.py
+++ app/BasicCalculator.py
@@ -6,7 +6,6 @@ su = n1-n2
 mu = n1*n2
 d = n1/n2
 di = n1//n2
-power = n1**n2
 rqn1 = n1**(1/2)
 rqn2 = n2**(1/2)
 rcn1 = n1**(1/3)
@@ -16,7 +15,6 @@ r4n2 = n2**(1/4)
 print('The sum of {} and {} is {},'.format(n1, n2, s))
 print('The subtraction is {}, the product is {},'.format(su, mu))
 print('The division is {}, the integer division is {},'.format(d, di))
-print('{} to the power of {} equals {},'.format(n1, n2, power))
 print('The square root of {} equals {},'.format(n1, rqn1))
 print('The square root of {} equals {},'.format(n2, rqn2))
 print('The cubic root of {} equals {},'.format(n1, rcn1))
